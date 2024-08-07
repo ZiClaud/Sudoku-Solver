d7e3a388712d0cacf9e61bce104f017d00e3a39b
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2024-08-07 19:10:01 +0200

    Working on solving function(s)

--- main.c
+++ main.c
@@ -16,6 +16,18 @@ int sudoku[9][9] = {
     {9, 0, 1, 0, 0, 0, 0, 6, 0}
 };
 
+int sudoku2[9][9] = {
+    {1, 2, 3, 4, 5, 0, 7, 8, 9},
+    {2, 3, 4, 5, 6, 7, 8, 9, 1},
+    {3, 4, 5, 6, 7, 8, 9, 1, 2},
+    {4, 5, 6, 7, 8, 9, 1, 2, 3},
+    {5, 6, 7, 8, 9, 1, 2, 3, 4},
+    {6, 7, 8, 9, 1, 2, 3, 4, 5},
+    {7, 8, 9, 1, 2, 3, 4, 5, 6},
+    {8, 9, 1, 2, 3, 4, 5, 6, 7},
+    {9, 1, 2, 3, 4, 5, 6, 7, 8}
+};
+
 int possible_positions[9][9][9];
 
 void fill_with_zeros(int s[9][9]) {
@@ -51,26 +63,48 @@ void print_sudoku(int s[9][9]) {
         }
     }
 
+    printf("\n\n");
+
     // printf("%d", s[1][0]); // 4
     // printf("%d", s[0][1]); // 1
 }
 
-void _remove_row(int poss[9][9][9], int row, int col) {
+bool contains(int list[9], int val) {
+    for (int i = 0; i < 9; ++i) {
+        if (list[i] == val) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void _remove(int list[9], int val) {
+    list[val - 1] = 0;
+}
+
+void _remove_row(int s[9][9], int poss[9][9][9], int row, int col) {
     // If item in row gets removed, -> number_was_removed = true
+    for (int c = 0; c < 9; ++c) {
+        if (contains(poss[row][col], s[row][c])) {
+            number_was_removed = true;
+            _remove(poss[row][col], s[row][c]);
+        }
+    }
 }
 
-void _remove_col(int poss[9][9][9], int row, int col) {
+void _remove_col(int s[9][9], int poss[9][9][9], int row, int col) {
     // If item in col gets removed, -> number_was_removed = true
-}
-
-void remove_row_and_col(int poss[9][9][9], int row, int col) {
-    _remove_row(poss, row, col);
-    _remove_col(poss, row, col);
-}
-
-void remove_square(int poss[9][9][9], int row, int col) {
     // TODO
+}
+
+void remove_row_and_col(int s[9][9], int poss[9][9][9], int row, int col) {
+    _remove_row(s, poss, row, col);
+    _remove_col(s, poss, row, col);
+}
+
+void remove_square(int s[9][9], int poss[9][9][9], int row, int col) {
     // If item in square gets removed, -> number_was_removed = true
+    // TODO
 }
 
 void solve_easy(int s[9][9]) {
@@ -79,8 +113,9 @@ void solve_easy(int s[9][9]) {
     for (int row = 0; row < 9; ++row) {
         for (int col = 0; col < 9; ++col) {
             if (s[row][col] == 0) {
-                remove_row_and_col(possible_positions, row, col);
+                remove_row_and_col(s, possible_positions, row, col);
             } else {
+                // Sets all values to -1
                 for (int i = 0; i < 9; ++i) {
                     possible_positions[row][col][i] = -1;
                 }
@@ -94,30 +129,36 @@ void solve_easy(int s[9][9]) {
 
     // Add all possible positions where theres just a number that fits there
     int count;
+    int val;
     for (int row = 0; row < 9; ++row) {
         count = 0;
+        val = -1;
         for (int col = 0; col < 9; ++col) {
             for (int i = 0; i < 9; ++i) {
                 if (possible_positions[row][col][i]) {
                     count++;
+                    val = possible_positions[row][col][i];
                 }
             }
-        }
-        if (count == 1) {
-            // TODO -> Set the only number in sudoku
+            if (count == 1) {
+                s[row][col] = val;
+            }
         }
     }
 }
 
 int main(void) {
-    print_sudoku(sudoku);
+    printf("Start:\n\n");
+    print_sudoku(sudoku2);
     fill_with_every_number(possible_positions);
 
     do {
-        solve_easy(sudoku);
+        solve_easy(sudoku2);
     } while (number_was_removed);
 
-    print_sudoku(sudoku);
+
+    printf("Solved:\n\n");
+    print_sudoku(sudoku2);
     // print_sudoku(possible_positions);
 
 
