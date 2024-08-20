b93fccbb4ecbfd7f104974572925cc244f3bb673
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2024-08-20 13:35:20 +0200

    Made print prettier Improved README

--- README.md
+++ README.md
@@ -2,4 +2,12 @@
 
 ### Made in C
 
-Sudoku examples in folder `examples` are from https://opensudoku.moire.org/
+You can test the solver by using the strings found in the `*.json` files inside the directory `examples`.
+
+- If the text at the end appears to be `white`, it was solved successfully
+- If the text at the end appears to be `cyan`, it couldn't complete the whole sudoku
+- If the text at the end appears to be `red`, there were some errors made while solving the sudoku
+
+---
+
+Sudoku examples in directory `examples` are from https://opensudoku.moire.org/
--- main.c
+++ main.c
@@ -6,6 +6,27 @@
 #include "solvers/med_solver.h"
 #include "examples/test.h"
 
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+void print_result(int sudoku[9][9], int solved, int to_solve) {
+    print_sudoku(sudoku);
+    if (has_errors(sudoku)) {
+        printf(ANSI_COLOR_YELLOW "WARNING: Errors found" ANSI_COLOR_RESET "\n");
+        printf(ANSI_COLOR_RED "Solved %d/%d:\n"ANSI_COLOR_RESET "\n", solved, to_solve);
+    } else {
+        if (solved == to_solve) {
+            printf("Solved %d/%d:\n\n", solved, to_solve);
+        } else {
+            printf(ANSI_COLOR_CYAN "Solved %d/%d:\n"ANSI_COLOR_RESET "\n", solved, to_solve);
+        }
+    }
+}
 
 int main(void) {
     int sudoku[9][9];
@@ -30,11 +51,7 @@ int main(void) {
         solve_med(sudoku, possible_positions, &is_changed, &solved);
     } while (is_changed);
 
-    print_sudoku(sudoku);
-    if (has_errors(sudoku)) {
-        printf("WARNING: Errors found\n");
-    }
-    printf("Solved %d/%d:\n\n", solved, to_solve);
+    print_result(sudoku, solved, to_solve);
 
     return 0;
 }
