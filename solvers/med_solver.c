3f622e44a5b319faf59a8f15d74b6a6f94dd7577
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2024-09-19 20:43:58 +0200

    Med solver now works for the box too

--- solvers/med_solver.c
+++ solvers/med_solver.c
@@ -5,8 +5,10 @@
 #include "med_solver.h"
 
 #include <assert.h>
+#include <stdio.h>
 
 #include "basic_solver.h"
+#include "../basics/basics.h"
 #include "../basics/basic_operations.h"
 #include "../basics/poss_pos_operations.h"
 
@@ -57,9 +59,54 @@ void _only_col(int s[9][9], int poss_pos[9][9][9], bool *is_changed, int *solved
     }
 }
 
+/// Returns the number of the vals inside the box (>= 1)
+int _val_inside_box(int poss_pos[9][9][9], const int row, const int col, const int val) {
+    int ris = 0;
+    int t_row = row / 3;
+    int t_col = col / 3;
+
+    t_row *= 3;
+    t_col *= 3;
+
+    for (int r = t_row; r < t_row + 3; ++r) {
+        for (int c = t_col; c < t_col + 3; ++c) {
+            if (contains(poss_pos[r][c], val)) {
+                ris++;
+            }
+        }
+    }
+
+    assert(ris != 0);
+    return ris;
+}
+
 /// Inserts value in [s] if it's the only one in the box of [poss_pos]
+void _check_box(int s[9][9], int poss_pos[9][9][9], const int row, const int col, bool *is_changed, int *solved_num) {
+    for (int i = 0; i < 9; ++i) {
+        const int val = poss_pos[row][col][i];
+        if (val != VOID_CELL) {
+            if (_val_inside_box(poss_pos, row, col, val) == 1) {
+                insert_value(s, poss_pos, row, col, val, solved_num);
+                *is_changed = true;
+                remove_box(s, poss_pos, row, col, is_changed);
+                return;
+            }
+        }
+    }
+}
+
+/// Checks if every single value inside the box does appear only once
 void _only_box(int s[9][9], int poss_pos[9][9][9], bool *is_changed, int *solved_num) {
-    // TODO
+    for (int row = 0; row < 9; ++row) {
+        for (int col = 0; col < 9; ++col) {
+            if (s[row][col] == 0) {
+                _check_box(s, poss_pos, row, col, is_changed, solved_num);
+                if (*is_changed) {
+                    return;
+                }
+            }
+        }
+    }
 }
 
 /// Places all [poss_pos] numbers that are the only ones in the same row/col/box
